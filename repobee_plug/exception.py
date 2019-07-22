c6311f0a197b15681a7fb9435075c2c3f4a05837
Simon Larsén <slarse@kth.se>
2019-07-22 11:42:20 +0200

    Move apimeta to repobee-plug

--- repobee_plug/exception.py
+++ repobee_plug/exception.py
@@ -21,3 +21,7 @@ class ExtensionCommandError(PlugError):
     """Raise when an :py:class:~repobee_plug.containers.ExtensionCommand: is
     incorrectly defined.
     """
+
+
+class APIImplementationError(PlugError):
+    """Raise when an API is defined incorrectly."""
