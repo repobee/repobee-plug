bdd853098d952bae4b91aedbe11a17b28237e719
Simon Larsén <slarse@kth.se>
2019-07-22 12:04:33 +0200

    [feat] Move apimeta to repobee-plug (#23)

--- setup.py
+++ setup.py
@@ -1,43 +1,45 @@
 import re
 from setuptools import setup, find_packages
 
-with open('README.md', mode='r', encoding='utf-8') as f:
+with open("README.md", mode="r", encoding="utf-8") as f:
     readme = f.read()
 
 # parse the version instead of importing it to avoid dependency-related crashes
-with open('repobee_plug/__version.py', mode='r', encoding='utf-8') as f:
+with open("repobee_plug/__version.py", mode="r", encoding="utf-8") as f:
     line = f.readline()
     __version__ = line.split("=")[1].strip(" '\"\n")
-    assert re.match(r"^\d+(\.\d+){2}$", __version__)
+    assert re.match(r"^\d+(\.\d+){2}(-(alpha|beta|rc)(\.\d+)?)?$", __version__)
 
-test_requirements = ['pytest>=4.0.0', 'pytest-cov', 'codecov', 'tox']
-required = ['pluggy>=0.8.0']
+test_requirements = ["pytest>=4.0.0", "pytest-cov", "codecov", "tox"]
+required = ["pluggy>=0.8.0"]
 
 setup(
-    name='repobee-plug',
+    name="repobee-plug",
     version=__version__,
-    description='Core components for plugin system in repobee',
+    description="Core components for plugin system in repobee",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Simon Larsén',
-    author_email='slarse@kth.se',
-    url='https://github.com/repobee/repobee-plug',
-    download_url='https://github.com/repobee/repobee-plug/archive/v{}.tar.gz'.
-    format(__version__),
-    license='MIT',
-    packages=find_packages(exclude=('tests', 'docs')),
+    long_description_content_type="text/markdown",
+    author="Simon Larsén",
+    author_email="slarse@kth.se",
+    url="https://github.com/repobee/repobee-plug",
+    download_url="https://github.com/repobee/repobee-plug/archive/v{}.tar.gz".format(
+        __version__
+    ),
+    license="MIT",
+    packages=find_packages(exclude=("tests", "docs")),
     tests_require=test_requirements,
     install_requires=required,
     extras_require=dict(TEST=test_requirements),
     include_package_data=True,
     zip_save=False,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Education',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
-    ])
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+    ],
+)
